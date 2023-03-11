7e835a9398a60aea452b03803594bb6f77958d52
Dave O <davidobrite@gmail.com>
2023-03-11 10:51:26 -0800

    Rename beat_nanos to beat_at

--- lib/timer/periodic.rb
+++ lib/timer/periodic.rb
@@ -1,24 +1,24 @@
 module Timer
   # Periodic Timers
   class Periodic
-    attr_reader :curr_beat_nanos, :next_beat_nanos
+    attr_reader :curr_beat_at, :next_beat_at
 
     def initialize(bpm, now_ns)
       @bpm = bpm
-      @curr_beat_nanos = now_ns
-      @next_beat_nanos = curr_beat_nanos + bpm.nanos_per_beat
+      @curr_beat_at = now_ns
+      @next_beat_at = curr_beat_at + bpm.nanos_per_beat
     end
 
     def update(now_ns, triggerable)
-      return unless now_ns >= curr_beat_nanos
+      return unless now_ns >= curr_beat_at
 
       triggerable.trigger
       step
     end
 
     def step
-      @curr_beat_nanos = next_beat_nanos
-      @next_beat_nanos = curr_beat_nanos + bpm.nanos_per_beat
+      @curr_beat_at = next_beat_at
+      @next_beat_at = curr_beat_at + bpm.nanos_per_beat
     end
 
     private
--- test/timer/test_periodic.rb
+++ test/timer/test_periodic.rb
@@ -4,47 +4,47 @@ module Timer
   class TestBpm < Minitest::Test
     include StubNow
 
-    def test_new_curr_beat_nanos
+    def test_new_curr_beat_at
       stub_now do
         now_ns = Nanos.now.value
         bpm = Bpm.new(120)
         periodic = Periodic.new(bpm, now_ns)
         expected = NOW_IN_NANOS
 
-        assert_equal expected, periodic.curr_beat_nanos
+        assert_equal expected, periodic.curr_beat_at
       end
     end
 
-    def test_new_next_beat_nanos
+    def test_new_next_beat_at
       stub_now do
         now_ns = Nanos.now.value
         bpm = Bpm.new(120)
         periodic = Periodic.new(bpm, now_ns)
         expected = NOW_IN_NANOS + (0.5 * NANOS_PER_SECOND).floor
 
-        assert_equal expected, periodic.next_beat_nanos
+        assert_equal expected, periodic.next_beat_at
       end
     end
 
-    def test_step_curr_beat_nanos
+    def test_step_curr_beat_at
       stub_now do
         now_ns = Nanos.now.value
         bpm = Bpm.new(120)
         periodic = Periodic.new(bpm, now_ns).tap(&:step)
         expected = NOW_IN_NANOS + (0.5 * NANOS_PER_SECOND).floor
 
-        assert_equal expected, periodic.curr_beat_nanos
+        assert_equal expected, periodic.curr_beat_at
       end
     end
 
-    def test_step_next_beat_nanos
+    def test_step_next_beat_at
       stub_now do
         now_ns = Nanos.now.value
         bpm = Bpm.new(120)
         periodic = Periodic.new(bpm, now_ns).tap(&:step)
         expected = NOW_IN_NANOS + (1.0 * NANOS_PER_SECOND).floor
 
-        assert_equal expected, periodic.next_beat_nanos
+        assert_equal expected, periodic.next_beat_at
       end
     end
   end
