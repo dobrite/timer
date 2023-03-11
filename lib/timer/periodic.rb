86e607eff8ee971e9cb383dd91f95072db725730
Dave O <davidobrite@gmail.com>
2023-03-11 11:16:22 -0800

    Add mult to periodic

--- lib/timer/periodic.rb
+++ lib/timer/periodic.rb
@@ -3,10 +3,11 @@ module Timer
   class Periodic
     attr_reader :prev_beat_at, :next_beat_at
 
-    def initialize(bpm, now_ns)
+    def initialize(bpm, now_ns, mult: 1)
       @bpm = bpm
+      @mult = mult
       @prev_beat_at = now_ns
-      @next_beat_at = prev_beat_at + bpm.nanos_per_beat
+      @next_beat_at = calc_next_beat_at
     end
 
     def update(now_ns, triggerable)
@@ -18,11 +19,15 @@ module Timer
 
     def step
       @prev_beat_at = next_beat_at
-      @next_beat_at = prev_beat_at + bpm.nanos_per_beat
+      @next_beat_at = calc_next_beat_at
     end
 
     private
 
-    attr_reader :bpm
+    def calc_next_beat_at
+      prev_beat_at + (bpm.nanos_per_beat * (1 / mult.to_f)).floor
+    end
+
+    attr_reader :bpm, :mult
   end
 end
--- test/timer/test_periodic.rb
+++ test/timer/test_periodic.rb
@@ -47,5 +47,16 @@ module Timer
         assert_equal expected, periodic.next_beat_at
       end
     end
+
+    def test_new_next_beat_at_mult2
+      stub_now do
+        now_ns = Nanos.now.value
+        bpm = Bpm.new(120)
+        periodic = Periodic.new(bpm, now_ns, mult: 4)
+        expected = NOW_IN_NANOS + (0.125 * NANOS_PER_SECOND).floor
+
+        assert_equal expected, periodic.next_beat_at
+      end
+    end
   end
 end
