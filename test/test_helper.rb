d06188f3c27622a4e3f1f62ce9589e509cdf6489
Dave O <davidobrite@gmail.com>
2023-03-11 07:43:21 -0800

    Add StubNow module to freeze time for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,14 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "timer"
 
 require "minitest/autorun"
+
+module StubNow
+  # 2023-03-11 07:27:49 2284889/4194304 -0800
+  FROZEN_IN_TIME = 1678548469.54476
+
+  def stub_now
+    Time.stub :now, Time.at(FROZEN_IN_TIME) do
+      yield
+    end
+  end
+end
