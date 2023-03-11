5a0df1765f608fd4abfce766734f95d11028e4fc
Dave O <davidobrite@gmail.com>
2023-03-11 10:52:38 -0800

    Rename curr_beat_at to prev_beat_at

--- lib/timer/periodic.rb
+++ lib/timer/periodic.rb
@@ -1,24 +1,24 @@
 module Timer
   # Periodic Timers
   class Periodic
-    attr_reader :curr_beat_at, :next_beat_at
+    attr_reader :prev_beat_at, :next_beat_at
 
     def initialize(bpm, now_ns)
       @bpm = bpm
-      @curr_beat_at = now_ns
-      @next_beat_at = curr_beat_at + bpm.nanos_per_beat
+      @prev_beat_at = now_ns
+      @next_beat_at = prev_beat_at + bpm.nanos_per_beat
     end
 
     def update(now_ns, triggerable)
-      return unless now_ns >= curr_beat_at
+      return unless now_ns >= prev_beat_at
 
       triggerable.trigger
       step
     end
 
     def step
-      @curr_beat_at = next_beat_at
-      @next_beat_at = curr_beat_at + bpm.nanos_per_beat
+      @prev_beat_at = next_beat_at
+      @next_beat_at = prev_beat_at + bpm.nanos_per_beat
     end
 
     private
--- test/timer/test_periodic.rb
+++ test/timer/test_periodic.rb
@@ -4,14 +4,14 @@ module Timer
   class TestBpm < Minitest::Test
     include StubNow
 
-    def test_new_curr_beat_at
+    def test_new_prev_beat_at
       stub_now do
         now_ns = Nanos.now.value
         bpm = Bpm.new(120)
         periodic = Periodic.new(bpm, now_ns)
         expected = NOW_IN_NANOS
 
-        assert_equal expected, periodic.curr_beat_at
+        assert_equal expected, periodic.prev_beat_at
       end
     end
 
@@ -26,14 +26,14 @@ module Timer
       end
     end
 
-    def test_step_curr_beat_at
+    def test_step_prev_beat_at
       stub_now do
         now_ns = Nanos.now.value
         bpm = Bpm.new(120)
         periodic = Periodic.new(bpm, now_ns).tap(&:step)
         expected = NOW_IN_NANOS + (0.5 * NANOS_PER_SECOND).floor
 
-        assert_equal expected, periodic.curr_beat_at
+        assert_equal expected, periodic.prev_beat_at
       end
     end
 
