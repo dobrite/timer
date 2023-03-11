c27c6ab6b7653ac93c3f9454e65d27ad9873134d
Dave O <davidobrite@gmail.com>
2023-03-11 08:28:32 -0800

    Fix rubocops; rubocop to 3.2

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,12 +7,10 @@ require "minitest/autorun"
 
 module StubNow
   # 2023-03-11 07:27:49 2284889/4194304 -0800
-  FROZEN_IN_TIME = 1678548469.54476
+  FROZEN_IN_TIME = 1_678_548_469.54_476
   NOW_IN_NANOS = 1_678_548_469_544_759_988
 
-  def stub_now
-    Time.stub :now, Time.at(FROZEN_IN_TIME) do
-      yield
-    end
+  def stub_now(&)
+    Time.stub(:now, Time.at(FROZEN_IN_TIME), &)
   end
 end
