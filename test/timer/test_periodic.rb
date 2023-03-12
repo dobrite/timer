cc670647824088c8ffb64840212c7fc36a161c92
Dave O <davidobrite@gmail.com>
2023-03-12 07:36:54 -0700

    chore(tests): convert periodic test now to even value

--- test/timer/test_periodic.rb
+++ test/timer/test_periodic.rb
@@ -5,15 +5,25 @@ module Timer
     include StubNow
 
     def test_new_next_beat_at
-      stub_now do
+      stub_now now do
         now = Nanos.now
         bpm = Bpm.new(120)
         bpm.start(now)
         periodic = Periodic.new(bpm)
-        expected = Nanos.from(NOW_IN_NANOS)
+        expected = now_in_nanos
 
         assert_equal expected, periodic.next_beat_at(now)
       end
+    end
+
+    private
+
+    def now
+      Time.new(2023, 1, 1, 12, 0, 0, "UTC").to_i
+    end
+
+    def now_in_nanos
+      Nanos.new(sec: 1_672_574_400, nsec: 0)
     end
   end
 end
