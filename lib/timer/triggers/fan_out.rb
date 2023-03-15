7afd8b65f4b5be29e412f3e231f5cde916930440
Dave O <davidobrite@gmail.com>
2023-03-14 17:44:35 -0700

    feat(fan_out): add an optional final_trigger

--- lib/timer/triggers/fan_out.rb
+++ lib/timer/triggers/fan_out.rb
@@ -2,17 +2,19 @@ module Timer
   module Triggers
     # A trigger that triggers a collection of triggerables
     class FanOut
-      def initialize(triggers)
+      def initialize(triggers, final_trigger: Triggers::Noop.new)
         @triggers = triggers
+        @final_trigger = final_trigger
       end
 
       def trigger
         triggers.each(&:trigger)
+        final_trigger.trigger
       end
 
       private
 
-      attr_reader :triggers
+      attr_reader :final_trigger, :triggers
     end
   end
 end
